fa06b981fced4ae146fd13e87788824ab861c0b0
Youssef AIT ALI <youssef.aitali@outlook.com>
2020-09-26 07:39:19 +0100

    listing, adding, editing and reacting to posts

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,86 @@
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
+import { client } from '../../api/client';
+
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts');
+  return response.posts;
+});
+
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  async initialPost => {
+    const response = await client.post('/fakeApi/posts', { post: initialPost });
+    return response.post;
+  }
+);
+
+const postsSlice = createSlice(
+    {
+        name: 'posts',
+        initialState,
+        reducers: {
+            reactionAdded(state, action) {
+                const { postId, reaction } = action.payload;
+                const existingPost = state.posts.find(post => post.id === postId);
+                if (existingPost) {
+                  existingPost.reactions[reaction]++;
+                }
+            },
+            postAdded: {
+                reducer(state, action) {
+                  state.posts.push(action.payload)
+                },
+                prepare(title, content, userId) {
+                  return {
+                    payload: {
+                      id: nanoid(),
+                      date: new Date().toISOString(),
+                      title,
+                      content,
+                      user: userId
+                    }
+                  }
+                }
+              },
+            postUpdated(state, action) {
+                const { id, title, content } = action.payload;
+                const existingPost = state.posts.find(post => post.id === id);
+                if(existingPost) {
+                    existingPost.title = title;
+                    existingPost.content = content;
+                }
+            }
+        },
+        extraReducers: {
+          [fetchPosts.pending]: (state, action) => {
+            state.status = 'loading'
+          },
+          [fetchPosts.fulfilled]: (state, action) => {
+            state.status = 'succeeded'
+            // Add any fetched posts to the array
+            state.posts = state.posts.concat(action.payload)
+          },
+          [fetchPosts.rejected]: (state, action) => {
+            state.status = 'failed'
+            state.error = action.error.message
+          },
+          [addNewPost.fulfilled]: (state, action) => {
+            state.posts.push(action.payload)
+          }
+        }
+    }
+)
+
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions;
+
+export default postsSlice.reducer;
+
+export const selectAllPosts = state => state.posts.posts;
+
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import store from './app/store'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './app/store';
+import { Provider } from 'react-redux';
 
-import './api/server'
+import { fetchUsers } from './features/users/usersSlice';
+
+import './api/server';
+
+store.dispatch(fetchUsers());
 
 ReactDOM.render(
   <React.StrictMode>
